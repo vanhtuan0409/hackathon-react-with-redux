0b1aa5a2bfab0a00fb61d910deff97cbc456434c
Tuấn Vương <vanhtuan0409@gmail.com>
2017-12-06 19:42:52 +0700

    add intro page

---
+++ src/containers/OrderForm/steps/Intro/index.js
@@ -0,0 +1,61 @@
+import React, { PureComponent } from "react";
+import appIcon from "@assets/img/appicon.png";
+import tchIcon from "@assets/img/tch.jpg";
+import "./style.styl";
+
+export default class Intro extends PureComponent {
+  render() {
+    const { next, previous } = this.props;
+    return (
+      <div className="intro-step">
+        <div className="install-app">
+          <img alt="app-icon" src={appIcon} />
+          The Coffee House Mobile
+          <a className="btn-install">Cài đặt ngay</a>
+        </div>
+
+        <div className="content">
+          <div className="head">
+            <h3>ĐẶT HÀNG VÀ GIAO TẬN NƠI TẠI</h3>
+            <h2>THE COFFEE HOUSE DELIVERY</h2>
+            <p>
+              Đừng quên
+              <em> Đăng nhập </em>
+              vào tài khoản
+              <em> The Coffee House Rewards </em>
+              của bạn để tích điểm và nhận các ưu đãi nhé!
+            </p>
+          </div>
+
+          <div className="body">
+            <img alt="tch-icon" src={tchIcon} />
+
+            <div className="order-steps">
+              <h3>Các bước đặt món</h3>
+
+              <ol>
+                <li>
+                  Điền thông tin nhận hàng (tên, số điện thoại, địa chỉ nhận
+                  hàng)
+                </li>
+                <li>Chọn món yêu thích</li>
+                <li>
+                  Tối đa 30 phút <span className="legend"> (*) </span>bạn sẽ có
+                  ngay món uống yêu thích!
+                </li>
+              </ol>
+            </div>
+
+            <div className="notes">
+              <p>
+                (*) Thời gian có thể khác nhau tùy thuộc vào vị trí và giao
+                thông tại thời điểm đặt hàng
+              </p>
+              <p>Thời gian phục vụ : 7h-20h</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
---
+++ src/containers/OrderForm/steps/common/NextButton.js
@@ -0,0 +1,7 @@
+import React, { PureComponent } from "react";
+
+export default class NextButton extends PureComponent {
+  render() {
+    return <div />;
+  }
+}
