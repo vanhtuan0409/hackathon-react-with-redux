2e288357d5ad4c3a35954e22c3b78044486cc7de
Tuấn Vương <vanhtuan0409@gmail.com>
2017-12-06 21:04:21 +0700

    improve wizard form

--- src/containers/OrderForm/OrderForm.js
+++ src/containers/OrderForm/OrderForm.js
@@ -1,41 +1,57 @@
 import React, { PureComponent } from "react";
 import { Actions } from "jumpstate";
 import Intro from "./steps/Intro";
+import CustomerInfo from "./steps/CustomerInfo";
 import Location from "./steps/Location";
 import SelectItem from "./steps/SelectItem";
 import Summary from "./steps/Summary";
 import "./style.styl";
 
+const steps = [
+  {
+    id: 1,
+    title: "Giới thiệu",
+    component: Intro
+  },
+  {
+    id: 2,
+    title: "Nhập thông tin đặt hàng",
+    component: CustomerInfo
+  },
+  {
+    id: 3,
+    title: "Chọn vị trí giao hàng",
+    component: Location
+  },
+  {
+    id: 4,
+    title: "Chọn món",
+    component: SelectItem
+  },
+  {
+    id: 5,
+    title: "Xem giỏ hàng",
+    component: Summary
+  }
+];
+
 export default class OrderForm extends PureComponent {
   render() {
-    const { currentStep } = this.props;
+    const { currentStep, data } = this.props;
+    const step = steps[currentStep - 1];
+    const { title, component: Component, validate } = step;
+    const isError = validate && !validate(data);
+    const nextFn =
+      currentStep === steps.length
+        ? Actions.form.submit
+        : Actions.form.goToNextStep;
     return (
-      <div className="order-form">
-        {currentStep === 1 && (
-          <Intro
-            previous={Actions.form.goToPreviousStep}
-            next={Actions.form.goToNextStep}
-          />
-        )}
-        {currentStep === 2 && (
-          <Location
-            previous={Actions.form.goToPreviousStep}
-            next={Actions.form.goToNextStep}
-          />
-        )}
-        {currentStep === 3 && (
-          <SelectItem
-            previous={Actions.form.goToPreviousStep}
-            next={Actions.form.goToNextStep}
-          />
-        )}
-        {currentStep === 4 && (
-          <Summary
-            previous={Actions.form.goToPreviousStep}
-            next={Actions.form.goToNextStep}
-          />
-        )}
-      </div>
+      <Component
+        title={title}
+        isError={isError}
+        previous={Actions.form.goToPreviousStep}
+        next={nextFn}
+      />
     );
   }
 }
--- src/modules/Form.js
+++ src/modules/Form.js
@@ -3,18 +3,24 @@ import { State, Effect, Actions } from "jumpstate";
 
 export default State("form", {
   initial: {
-    currentStep: 1
+    currentStep: 1,
+    data: {}
   },
 
   // Reducer for setUser action
   goToNextStep(state) {
-    if (state.currentStep >= 4) return state;
+    if (state.currentStep >= 5) return state;
     return Object.assign({}, state, { currentStep: state.currentStep + 1 });
   },
 
   goToPreviousStep(state) {
     if (state.currentStep <= 1) return state;
     return Object.assign({}, state, { currentStep: state.currentStep - 1 });
+  },
+
+  submit(state) {
+    alert(JSON.stringify(state.data));
+    return state;
   }
 });
 
