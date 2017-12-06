cc26a3fa4d5c3dab053f99b10dcf56b19b7cd3f9
Tuấn Vương <vanhtuan0409@gmail.com>
2017-12-06 19:01:57 +0700

    add some comment

--- src/modules/Auth.js
+++ src/modules/Auth.js
@@ -7,21 +7,25 @@ export default State("auth", {
     modalVisibility: false
   },
 
+  // Reducer for setUser action
   setUser(state, user) {
     return Object.assign({}, state, { user });
   },
 
+  // Reducer for showModal action
   showModal(state) {
     if (state.user) return;
     return Object.assign({}, state, { modalVisibility: true });
   },
 
+  // Reducer for closeModal action
   closeModal(state) {
     if (state.user) return;
     return Object.assign({}, state, { modalVisibility: false });
   }
 });
 
+// Reducer for login action
 Effect("login", async () => {
   const user = await authApi.doLogin();
   Actions.auth.setUser(user);
