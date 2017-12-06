5d07d4180874e71da0dde84b4b5890d6849b5d78
Tuấn Vương <vanhtuan0409@gmail.com>
2017-12-06 14:41:49 +0700

    initialize simple structure

---
+++ src/containers/App.js
@@ -0,0 +1,10 @@
+import React, { PureComponent } from "react";
+import "./App.css";
+
+class App extends PureComponent {
+  render() {
+    return <div>App</div>;
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import "./globalStyles/index.css";
+import App from "./containers/App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
