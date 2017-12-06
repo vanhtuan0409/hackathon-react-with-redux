bb2f493f8500b3b3c232b57b3424745f262cbe88
Tuấn Vương <vanhtuan0409@gmail.com>
2017-12-06 15:17:45 +0700

    integrate stylus css

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -211,6 +211,22 @@ module.exports = {
             )
             // Note: this won't work without `new ExtractTextPlugin()` in `plugins`.
           },
+          {
+            test: /\.styl$/,
+            use: [
+              require.resolve("style-loader"),
+              {
+                loader: require.resolve("css-loader"),
+                options: {
+                  importLoaders: 1
+                }
+              },
+              {
+                loader: require.resolve("stylus-loader"),
+                options: {}
+              }
+            ]
+          },
           // "file" loader makes sure assets end up in the `build` folder.
           // When you `import` an asset, you get its filename.
           // This loader doesn't use a "test" so it will catch all modules
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import "./globalStyles/index.css";
+import "./globalStyles/index.styl";
 import App from "./containers/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
