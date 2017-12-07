42b3385c0974eb0f28bafa715f986e40b75b61b0
Tuấn Vương <vanhtuan0409@gmail.com>
2017-12-07 13:00:07 +0700

    initialize summary

--- src/containers/OrderForm/steps/Summary/index.js
+++ src/containers/OrderForm/steps/Summary/index.js
@@ -1,14 +1,33 @@
 import React, { PureComponent } from "react";
+import BottomButtomGroup from "@components/BottomButtonGroup";
+import Button from "@components/Button";
+import StepLayout from "../StepLayout";
+import "./style.styl";
 
 export default class Summary extends PureComponent {
   render() {
-    const { next, previous } = this.props;
+    const { data, next, previous } = this.props;
     return (
-      <div>
-        <p>Summary</p>
-        <button onClick={previous}>Go to previous</button>
-        <button onClick={next}>Go to next</button>
-      </div>
+      <StepLayout
+        className="summary-step"
+        title="Đơn hàng"
+        index={{ step: 4, total: 4 }}
+        onBack={previous}
+      >
+        <div className="summary-step-inner">
+          <div className="header-separator" />
+          asdasd
+        </div>
+
+        <BottomButtomGroup>
+          <Button standard onClick={previous}>
+            Chọn thêm món
+          </Button>
+          <Button primary onClick={next}>
+            Đặt hàng
+          </Button>
+        </BottomButtomGroup>
+      </StepLayout>
     );
   }
 }
